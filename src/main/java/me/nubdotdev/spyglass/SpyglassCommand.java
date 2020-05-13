f574c7fd4707cfd1c84f909a46c8ae885f8a43e0
nubDotDev <nubdotdev@gmail.com>
2020-05-13 13:27:45 -0400

    Send toggle message to target when someone else does the command Removed unnecessary check

--- src/main/java/me/nubdotdev/spyglass/SpyglassCommand.java
+++ src/main/java/me/nubdotdev/spyglass/SpyglassCommand.java
@@ -24,73 +24,72 @@ public class SpyglassCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("spyglass")) {
-            if (args.length >= 1) {
-                if (subCommands.contains(args[0].toLowerCase())) {
-                    if (args[0].equalsIgnoreCase("reload")) {
-                        if (sender.hasPermission("spyglass.reload")) {
-                            plugin.getConfigManager().reload();
-                            sender.sendMessage(plugin.getConfigManager().getMessage("reload"));
-                        } else {
-                            sender.sendMessage(plugin.getConfigManager().getMessage("no-perms"));
-                        }
+        if (args.length >= 1) {
+            if (subCommands.contains(args[0].toLowerCase())) {
+                if (args[0].equalsIgnoreCase("reload")) {
+                    if (sender.hasPermission("spyglass.reload")) {
+                        plugin.getConfigManager().reload();
+                        sender.sendMessage(plugin.getConfigManager().getMessage("reload"));
                     } else {
-                        String spy = args[0].toLowerCase();
-                        Player p;
-                        if (args.length >= 2 && !args[1].equalsIgnoreCase(sender.getName())) {
-                            if (!sender.hasPermission("spyglass." + spy + ".other")) {
-                                sender.sendMessage(plugin.getConfigManager().getMessage("no-perms"));
-                                return true;
-                            }
-                            p = Bukkit.getPlayer(args[1]);
-                            if (p == null) {
-                                sender.sendMessage(plugin.getConfigManager().getMessage("no-player"));
-                                return true;
-                            }
-                        } else if (sender instanceof Player) {
-                            if (!sender.hasPermission("spyglass." + spy + ".self")) {
-                                sender.sendMessage(plugin.getConfigManager().getMessage("no-perms"));
-                                return true;
-                            }
-                            p = (Player) sender;
-                        } else {
-                            sender.sendMessage("Only players can use that command!");
+                        sender.sendMessage(plugin.getConfigManager().getMessage("no-perms"));
+                    }
+                } else {
+                    String spy = args[0].toLowerCase();
+                    Player p;
+                    if (args.length >= 2 && !args[1].equalsIgnoreCase(sender.getName())) {
+                        if (!sender.hasPermission("spyglass." + spy + ".other")) {
+                            sender.sendMessage(plugin.getConfigManager().getMessage("no-perms"));
                             return true;
                         }
-                        String mode;
-                        if (spy.equals("command")) {
-                            if (plugin.getCommandSpy().contains(p)) {
-                                plugin.getCommandSpy().remove(p);
-                                mode = "disabled";
-                            } else {
-                                plugin.getCommandSpy().add(p);
-                                mode = "enabled";
-                            }
-                        } else {
-                            if (plugin.getSocialSpy().contains(p)) {
-                                plugin.getSocialSpy().remove(p);
-                                mode = "disabled";
-                            } else {
-                                plugin.getSocialSpy().add(p);
-                                mode = "enabled";
-                            }
+                        p = Bukkit.getPlayer(args[1]);
+                        if (p == null) {
+                            sender.sendMessage(plugin.getConfigManager().getMessage("no-player"));
+                            return true;
                         }
-                        sender.sendMessage(plugin.getConfigManager().getMessage("toggle")
-                                .replaceAll("%spy%", spy)
-                                .replaceAll("%mode%", mode)
-                                .replaceAll("%player%", p.getName())
-                        );
+                    } else if (sender instanceof Player) {
+                        if (!sender.hasPermission("spyglass." + spy + ".self")) {
+                            sender.sendMessage(plugin.getConfigManager().getMessage("no-perms"));
+                            return true;
+                        }
+                        p = (Player) sender;
+                    } else {
+                        sender.sendMessage("Only players can use that command!");
+                        return true;
                     }
-                    return true;
+                    String mode;
+                    if (spy.equals("command")) {
+                        if (plugin.getCommandSpy().contains(p)) {
+                            plugin.getCommandSpy().remove(p);
+                            mode = "disabled";
+                        } else {
+                            plugin.getCommandSpy().add(p);
+                            mode = "enabled";
+                        }
+                    } else {
+                        if (plugin.getSocialSpy().contains(p)) {
+                            plugin.getSocialSpy().remove(p);
+                            mode = "disabled";
+                        } else {
+                            plugin.getSocialSpy().add(p);
+                            mode = "enabled";
+                        }
+                    }
+                    String message = plugin.getConfigManager().getMessage("toggle")
+                            .replaceAll("%spy%", spy)
+                            .replaceAll("%mode%", mode)
+                            .replaceAll("%player%", p.getName());
+                    sender.sendMessage(message);
+                    if (!p.getName().equals(sender.getName()))
+                        p.sendMessage(message);
                 }
+                return true;
             }
-            if (sender.hasPermission("spyglass.help"))
-                sender.sendMessage(ChatColor.RED + "Usage: " + cmd.getUsage());
-            else
-                sender.sendMessage(plugin.getConfigManager().getMessage("no-perms"));
-            return true;
         }
-        return false;
+        if (sender.hasPermission("spyglass.help"))
+            sender.sendMessage(ChatColor.RED + "Usage: " + cmd.getUsage());
+        else
+            sender.sendMessage(plugin.getConfigManager().getMessage("no-perms"));
+        return true;
     }
 
     @Override
